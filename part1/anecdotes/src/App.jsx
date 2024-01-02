77f362d47182216c657b709b1d3f14c916afb467
nicolac <nicola.casagrande2000@gmail.com>
2024-01-02 15:56:12 +0100

    Part1 ex13

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -13,14 +13,23 @@ const App = () => {
   ]
    
   const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(Array(anecdotes.length).fill(0))
 
   function selectRandomAnecdote() {
     setSelected(Math.floor(Math.random() * anecdotes.length))
   }
 
+  function increaseVotes(){
+    const copy = [...votes]
+    copy[selected] += 1
+    setVotes(copy)
+  }
+
   return (
     <div>
       <p>{anecdotes[selected]}</p>
+      <p>has {votes[selected]} votes</p>
+      <button onClick={increaseVotes}>vote</button>
       <button onClick={selectRandomAnecdote}>next anecdote</button>
     </div>
   )
