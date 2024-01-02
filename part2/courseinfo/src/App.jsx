893a5d2ec0c476bd69c9384fee75a40fa1847f1b
nicolac <nicola.casagrande2000@gmail.com>
2024-01-02 18:59:22 +0100

    Part2 ex5

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,27 +1,4 @@
-const Course = ({ course }) => 
-  <>
-  <Header course={course.name} />
-  <Content parts={course.parts}></Content>
-  <Total sum={course.parts.reduce((sum, part) => sum + part.exercises, 0)} />
-  </>
-
-const Header = ({ course }) => <h1>{course}</h1>
-
-const Total = ({ sum }) => <p><strong>total of {sum} exercises</strong></p>
-
-const Part = ({ part }) => 
-  <p>
-    {part.name} {part.exercises}
-  </p>
-
-const Content = ({ parts }) => 
-  <>
-  {parts.map(part =>
-    <Part
-      key={part.id}
-      part={part} 
-    />)}    
-  </>
+import Course from './components/Course'
 
 const App = () => {
   const courses = [
@@ -77,7 +54,7 @@ const App = () => {
       course={course} 
     />)}
   </>)
-  
+
 }
 
 export default App
