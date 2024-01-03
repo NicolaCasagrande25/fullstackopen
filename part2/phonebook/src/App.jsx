df0188029ad71d8a510ec517835c4bc867beb0ff
nicolac <nicola.casagrande2000@gmail.com>
2024-01-03 14:13:47 +0100

    Part2 ex10

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,78 +1,73 @@
-import { useState } from 'react'
+import { useState } from "react"
+import Filter from "./components/Filter"
+import PersonForm from "./components/PersonForm"
+import Persons from "./components/Persons"
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
-  const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState('')
-  const [searchQuery, setSearchQuery] = useState('')
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
+  ]);
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+  const [searchQuery, setSearchQuery] = useState("");
 
-  const personsToShow = persons.filter(person => person.name.toLowerCase().includes(searchQuery.toLowerCase()))
+  const personsToShow = persons.filter((person) =>
+    person.name.toLowerCase().includes(searchQuery.toLowerCase())
+  );
 
   const handleNameChange = (event) => {
-    setNewName(event.target.value)
-  }
+    setNewName(event.target.value);
+  };
 
   const handleNumberChange = (event) => {
-    setNewNumber(event.target.value)
-  }
+    setNewNumber(event.target.value);
+  };
 
   const handleSearchQueryChange = (event) => {
-    setSearchQuery(event.target.value)
-  }
+    setSearchQuery(event.target.value);
+  };
 
   const addNewPerson = (event) => {
-    event.preventDefault()
-    
-    if(persons.find(person => person.name === newName)) {
-      alert(`${newName} is already added to phonebook`)
+    event.preventDefault();
+
+    if (persons.find((person) => person.name === newName)) {
+      alert(`${newName} is already added to phonebook`);
     } else {
       const newPerson = {
         name: newName,
-        number: newNumber
-      }
-      setPersons(persons.concat(newPerson))
+        number: newNumber,
+      };
+      setPersons(persons.concat(newPerson));
     }
-    setNewName('')
-    setNewNumber('')
-  }
+    setNewName("");
+    setNewNumber("");
+  };
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <div>
-          filter shown with: <input 
-            value={searchQuery} 
-            onChange={handleSearchQueryChange}
-          />
-      </div>
+      <Filter
+        searchQuery={searchQuery}
+        handleSearchQueryChange={handleSearchQueryChange}
+      ></Filter>
       <h2>add a new</h2>
-      <form onSubmit={addNewPerson}>
-        <div>
-          name: <input 
-            value={newName} 
-            onChange={handleNameChange}
-          />
-        </div>
-        <div>number: <input 
-          value={newNumber}
-          onChange={handleNumberChange}
-        />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
+      <PersonForm
+        addNewPerson={addNewPerson}
+        newName={newName}
+        handleNameChange={handleNameChange}
+        newNumber={newNumber}
+        handleNumberChange={handleNumberChange}
+      >
+      </PersonForm>
       <h2>Numbers</h2>
-      {personsToShow.map(person => 
-      <div key={person.name}>{person.name} {person.number}</div>
-      )}
+      <Persons
+        personsToShow={personsToShow}
+      ></Persons>
     </div>
-  )
-}
+  );
+};
 
 export default App
---
+++ part2/phonebook/src/components/Filter.jsx
@@ -0,0 +1,8 @@
+const Filter = ({ searchQuery, handleSearchQueryChange }) => (
+  <div>
+    filter shown with:{" "}
+    <input value={searchQuery} onChange={handleSearchQueryChange} />
+  </div>
+);
+
+export default Filter
