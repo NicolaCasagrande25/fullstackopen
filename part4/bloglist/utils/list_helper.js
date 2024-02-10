961e0a7344143377fd58ca5bbaaa4765e5a5fac9
nicolac <nicola.casagrande2000@gmail.com>
2024-02-10 17:40:22 +0200

    Part4 ex5

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -10,7 +10,26 @@ const totalLikes = (blogs) => {
     return blogs.reduce(reducer, 0)
 }
 
+const favoriteBlog = (blogs) => {
+    const reducer = (favoriteBlog, blog) => {
+        return favoriteBlog.likes > blog.likes ? favoriteBlog : blog
+    }
+
+    const favoriteBlog = blogs.length > 0 ? blogs.reduce(reducer, blogs[0]) : undefined
+    
+    if(favoriteBlog){
+        return {
+            title: favoriteBlog.title,
+            author: favoriteBlog.author,
+            likes: favoriteBlog.likes,
+        }
+    } else {
+        return undefined
+    }
+}
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
