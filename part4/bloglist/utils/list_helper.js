8b08156759b7128d6113bb6fc80855f658ce794e
nicolac <nicola.casagrande2000@gmail.com>
2024-02-10 20:03:38 +0200

    Part4 ex6

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,3 +1,5 @@
+const blog = require("../models/blog")
+
 const dummy = (blogs) => {
     return 1
 }
@@ -16,8 +18,8 @@ const favoriteBlog = (blogs) => {
     }
 
     const favoriteBlog = blogs.length > 0 ? blogs.reduce(reducer, blogs[0]) : undefined
-    
-    if(favoriteBlog){
+
+    if (favoriteBlog) {
         return {
             title: favoriteBlog.title,
             author: favoriteBlog.author,
@@ -28,8 +30,45 @@ const favoriteBlog = (blogs) => {
     }
 }
 
+// const mostBlogs = (blogs) => {
+//     const authorsWithBlogsNumber = [];
+
+//     const reducer = (mostBlogs, author) => {
+//         return mostBlogs.blogs > author.blogs ? mostBlogs : author
+//     }
+
+//     blogs.forEach(blog => {
+//         let author = authorsWithBlogsNumber.find(a => a.author === blog.author)
+//         if (author) {
+//             author.blogs += 1
+//         } else {
+//             authorsWithBlogsNumber.push({ author: blog.author, blogs: 1 })
+//         }
+//     })
+
+//     return authorsWithBlogsNumber.length > 0 ? authorsWithBlogsNumber.reduce(reducer, authorsWithBlogsNumber[0]) : undefined
+// }
+
+const mostBlogs = (blogs) => {
+    if (blogs.length === 0) {
+        return undefined
+    }
+
+    const blogsPerAuthorReducer = (blogsPerAuthorCounter, blog) => {
+        blogsPerAuthorCounter[blog.author] = (blogsPerAuthorCounter[blog.author] || 0) + 1
+        return blogsPerAuthorCounter
+    }
+
+    const blogsPerAuthor = blogs.reduce(blogsPerAuthorReducer, {})
+
+    return Object.entries(blogsPerAuthor).reduce((authorWithMostBlogs, [author, blogs]) => {
+        return authorWithMostBlogs.blogs > blogs ? authorWithMostBlogs : { author: author, blogs: blogs }
+    }, { author: '', blogs: 0 })
+}
+
 module.exports = {
     dummy,
     totalLikes,
-    favoriteBlog
+    favoriteBlog,
+    mostBlogs
 }
