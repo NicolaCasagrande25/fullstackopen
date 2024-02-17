7621278a77f3da8964afd85a76dd16d5b8c0e6ea
nicolac <nicola.casagrande2000@gmail.com>
2024-02-17 15:32:01 +0200

    Part4 ex15

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -3,6 +3,7 @@ const express = require('express')
 require('express-async-errors')
 const app = express()
 const cors = require('cors')
+const usersRouter = require('./controllers/users')
 const blogsRouter = require('./controllers/blogs')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
@@ -20,6 +21,7 @@ mongoose.connect(config.MONGODB_URI)
 app.use(cors())
 app.use(express.json())
 
+app.use('/api/users', usersRouter) 
 app.use('/api/blogs', blogsRouter)
 
 module.exports = app
