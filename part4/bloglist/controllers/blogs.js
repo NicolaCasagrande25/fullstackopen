9eb11ecd99002d3f502a3e5515f8e4f528b3a5e8
nicolac <nicola.casagrande2000@gmail.com>
2024-02-17 17:00:25 +0200

    Part4 ex17

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,8 +1,11 @@
 const blogsRouter = require('express').Router()
+const logger = require('../utils/logger')
 const Blog = require('../models/blog')
+const User = require('../models/user')
+const user = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog.find({}).populate('creator', { username: 1, name: 1 })
   response.json(blogs)
 })
 
@@ -14,7 +17,16 @@ blogsRouter.post('/', async (request, response) => {
   if (!blog.title || !blog.url) {
     return response.status(400).end()
   }
+
+  const users = await User.find({})
+  const user = users[0]
+  blog.creator = user._id
+
   const savedBlog = await blog.save()
+
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
+
   response.status(201).json(savedBlog)
 })
 
