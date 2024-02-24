25a5714a9cf08e9066fcbf72bceac87918ac5b87
nicolac <nicola.casagrande2000@gmail.com>
2024-02-24 16:37:03 +0200

    Part5 ex7

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,43 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react";
 
-export default Blog
+const Blog = ({ blog }) => {
+  const [showDetails, setShowDetails] = useState(false);
+
+  const hideWhenVisible = { display: showDetails ? "none" : "" };
+  const showWhenVisible = { display: showDetails ? "" : "none" };
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+  return (
+    <div style={blogStyle}>
+      {blog.title} {blog.author}
+      <button style={hideWhenVisible} onClick={() => setShowDetails(true)}>
+        view
+      </button>
+      <button style={showWhenVisible} onClick={() => setShowDetails(false)}>
+        hide
+      </button>
+      <div style={showWhenVisible}>
+        <span>{blog.url}</span>
+        <div>
+          likes {blog.likes}
+          <button
+            onClick={() => {
+              console.log(`you liked ${blog.title} by ${blog.author}`);
+            }}
+          >
+            like
+          </button>
+        </div>
+        <div>{blog.creator.name}</div>
+      </div>
+    </div>
+  );
+};
+
+export default Blog;
